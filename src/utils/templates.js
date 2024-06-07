53db51b7cb79b8ac898c1e032acc30750a1091de
Luis Masuelli <luisfmasuelli@gmail.com>
2024-06-07 11:19:36 -0500

    Adding a direct save after the replacement.

--- src/utils/templates.js
+++ src/utils/templates.js
@@ -3,9 +3,10 @@ import * as fs from "fs";
 
 const templatesDir = path.resolve(__dirname, "..", "..", "data", "generation", "templates");
 
-export function applyTemplate(filePath, replacements) {
-    const data = fs.readFileSync(path.resolve(templatesDir, filePath), 'utf8');
-    return data.replace(/#(\w+)#/g, (match, key) => {
+export function applyTemplate(filePath, replacements, toFilePath) {
+    const template = fs.readFileSync(path.resolve(templatesDir, filePath), {encoding: 'utf8'});
+    const data = template.replace(/#(\w+)#/g, (match, key) => {
         return replacements[key] !== undefined ? replacements[key] : match;
     });
+    fs.writeFileSync(toFilePath, data, {encoding: 'utf8'});
 }
