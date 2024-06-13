2c660b51c684c33dac12bac11d84bf7f72e35d88
Luis Masuelli <luisfmasuelli@gmail.com>
2024-06-12 22:23:03 -0500

    Adding non-interactive options for the generate-deployment cantrip.

--- src/tasks/generate-deployment.js
+++ src/tasks/generate-deployment.js
@@ -9,8 +9,11 @@ const {collectContractNames} = require("../utils/contracts");
 const {parseAddress} = require("../utils/accounts");
 
 
-async function selectContract(hre) {
+async function selectContract(hre, contractName) {
     const contractNames = collectContractNames(hre);
+    if (contractNames.find(({name}) => name === contractName)) {
+        return contractName;
+    }
 
     let uniqueChoices = [];
     let repeatedChoices = [];
@@ -46,32 +49,41 @@ async function selectContract(hre) {
 }
 
 
+function validateContractName(contractName) {
+    return /^[A-Za-z][A-Za-z0-9]*$/.test(contractName) ? contractName : ""
+}
+
+
 function inputDeploymentName(contractName) {
     return inputUntil(contractName, "Give a name to your deployment", (deploymentName) => {
-        return /[A-Za-z][A-Za-z0-9]*/.test(deploymentName);
+        return /^[A-Za-z][A-Za-z0-9]*$/.test(deploymentName);
     }, "Invalid deployment name.");
 }
 
 
 cantripsScope.task("generate-deployment", "Generates a deployment file for an existing contract")
+    .addOptionalParam("contractName", "An optional existing contract name")
+    .addOptionalParam("moduleName", "An optional ignition module name")
     .addOptionalParam("reference", "Generates an m.contractAt future to the specified address instead of an m.contract future")
     .addOptionalParam("chainId", "Whether to create this deployment chain-specific")
-    .setAction(async ({ reference, chainId }, hre, runSuper) => {
+    .setAction(async ({ contractName, reference, chainId, moduleName }, hre, runSuper) => {
         try {
+            contractName = (contractName || "").trim();
+            moduleName = (moduleName || "").trim();
             const ignitionPath = path.resolve(hre.config.paths.root, "ignition", "modules");
             await hre.run("compile");
-            const contractName = await selectContract(hre);
-            const deploymentName = await inputDeploymentName(contractName);
+            const contractName_ = await selectContract(hre, contractName);
+            const moduleName_ = validateContractName(moduleName) || await inputDeploymentName(contractName_);
 
             const sourceTemplate = reference ? "ignition/ContractReference.js.template" :
                 "ignition/ContractCreation.js.template";
             const parsedChainId = (chainId || 0) && parseChainId(chainId);
             const targetPath = path.resolve(
-                ignitionPath, parsedChainId ? `${deploymentName}-${parsedChainId}.js` : `${deploymentName}.js`
+                ignitionPath, parsedChainId ? `${moduleName_}-${parsedChainId}.js` : `${moduleName_}.js`
             );
             const replacements = {
-                MODULE_NAME: deploymentName,
-                CONTRACT_NAME: contractName,
+                MODULE_NAME: moduleName_,
+                CONTRACT_NAME: contractName_,
                 CONTRACT_ADDRESS: reference && parseAddress(reference, hre)
             }
             applyTemplate(sourceTemplate, replacements, targetPath);
