4f9bcbf148327982f908fd51dd643ab38782caea
Luis Masuelli <luisfmasuelli@gmail.com>
2024-06-13 15:35:39 -0500

    Adding some common filepath-related functions.

--- src/utils/common.js
+++ src/utils/common.js
@@ -31,6 +31,54 @@ function traverseDirectory(directory, callback)
 }
 
 
+/**
+ * Returns the base directory of the project. It removes any
+ * trailing slash.
+ * @param hre The hardhat runtime environment.
+ * @returns The project path.
+ */
+function getProjectPrefix(hre) {
+    let root = hre.config.paths.root;
+    while (root.endsWith('/')) root = root.substring(0, root.length - 1);
+    return root;
+}
+
+
+/**
+ * Removes the project prefix from the given file.
+ * @param file The (absolute) filename.
+ * @param hre The hardhat runtime environment.
+ * @returns A structure {file, stripped?}. If the file starts
+ * with the project prefix then stripped?=true and file= the
+ * file without the project prefix. Otherwise, stripped?=false
+ * and file=file.
+ */
+function removeProjectPrefix(file, hre) {
+    const prefix = getProjectPrefix(hre) + "/";
+    if (file.startsWith(prefix)) {
+        return {file: file.substring(prefix.length), stripped: true}
+    } else {
+        return {file, stripped: false};
+    }
+}
+
+
+/**
+ * Normalizes a filename with respect to the current project
+ * prefix (and returns the normalized filename or the base
+ * filename if not belonging to the project).
+ * @param file The (relative or absolute) filename.
+ * @param hre The hardhat runtime environment.
+ * @returns A structure {file, stripped?}. If the file belonged
+ * to the project (relatively or absolutely) returns its relative
+ * path and stripped?=true. Otherwise, returns its absolute path
+ * and stripped?=false.
+ */
+function normalizeByProjectPrefix(file, hre) {
+    return removeProjectPrefix(path.resolve(getProjectPrefix(hre), file), hre);
+}
+
+
 module.exports = {
-    traverseDirectory
+    traverseDirectory, getProjectPrefix, removeProjectPrefix, normalizeByProjectPrefix
 }
