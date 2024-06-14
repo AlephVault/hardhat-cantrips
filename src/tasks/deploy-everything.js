56320e4ae7f0eb66562d9f1f45dc7efcc044f764
Luis Masuelli <luisfmasuelli@gmail.com>
2024-06-14 09:50:24 -0500

    Starting the deploy-everything.js stub again.

--- src/tasks/deploy-everything.js
+++ src/tasks/deploy-everything.js
@@ -1,34 +1,128 @@
-const {applyTemplate} = require("../utils/templates");
 const enquirer = require("enquirer");
-const path = require("path");
 const {cantripsScope, inputUntil} = require("./common");
+const {checkNotInteractive} = require("../utils/common");
+
+
+/**
+ * Chooses an action out of the available ones.
+ * @param action The initial (and perhaps to keep) action. If not valid,
+ * this command tries to become interactive and pick one of the available
+ * actions (run, add, remove, list).
+ * @param forceNonInteractive If true, raises an error when the command tries
+ * to become interactive.
+ * @param hre The hardhat runtime environment.
+ * @returns {Promise<*|string>} The chosen action (async function).
+ */
+async function chooseAction(action, forceNonInteractive, hre) {
+    action = (action || "").trim().toLowerCase();
+    const actions = ["add", "remove", "list", "run"];
+    const choices = [
+        {name: "add", message: "Adds a new deployment module (prompted or via --module)"},
+        {name: "remove", message: "Removes a deployment module (prompted or via --module)"},
+        {name: "list", message: "Lists all the deployment modules (sequentially)"},
+        {name: "run", message: "Executes all the deployment modules (to the end)"}
+    ];
+    if (actions.indexOf(action) >= 0) {
+        return action;
+    }
+    if (action) {
+        console.error(`You've chosen an unsupported action: ${action}.`);
+    }
+
+    // Now, we'll prompt the user for the action to pick.
+    checkNotInteractive(forceNonInteractive);
+    let prompt = new enquirer.Select({
+        name: "action",
+        message: "Select what to do:",
+        choices
+    });
+    return await prompt.run();
+}
+
+
+/**
+ * Adds a module to the deployment.
+ * @param module The path to the module. If not given, this action tries
+ * to become interactive and:
+ * 1. For external modules: prompt the user to write the module path.
+ *    That file is import-tried globally.
+ * 2. For files: prompt the user to choose one of the available modules
+ *    inside the project's ignition/modules directory. That file is also
+ *    import-tried but locally to the project.
+ * @param external Whether it is an external path or a project-local one.
+ * @param forceNonInteractive If true, raises an error when the command tries
+ * to become interactive.
+ * @param hre The hardhat runtime environment.
+ * @returns {Promise<void>} Nothing (async function).
+ */
+async function add(module, external, forceNonInteractive, hre) {
+
+}
+
+
+/**
+ * Removes a module from the deployment.
+ * @param module The path to the module. If not given, this action tries
+ * to become interactive and list all the added modules (only those that
+ * are local or external, depending on whether the external argument is
+ * false or true, respectively).
+ * @param external Whether it is an external path or a project-local one.
+ * @param forceNonInteractive If true, raises an error when the command tries
+ * to become interactive.
+ * @param hre The hardhat runtime environment.
+ * @returns {Promise<void>} Nothing (async function).
+ */
+async function remove(module, external, forceNonInteractive, hre) {
+
+}
+
+
+/**
+ * Lists all the registered modules in the deployment.
+ * @param hre The hardhat runtime environment.
+ * @returns {Promise<void>} Nothing (async function).
+ */
+async function list(hre) {
+
+}
+
+
+/**
+ * Runs all the registered modules in the deployment.
+ * @param hre The hardhat runtime environment.
+ * @returns {Promise<void>} Nothing (async function).
+ */
+async function run(hre) {
+
+}
 
 
 cantripsScope.task("deploy-everything", "Manages or executes the 'full deployment' in a chain")
-    .addOptionalParam("add", "Adds a file to the deployment (might be a chain-specific file)")
-    .addFlag("imported", "When using --add, tells that the file is imported via require() instead of belonging to the project")
-    .addFlag("run", "Runs the full deployment. When not using --add, this is done by default")
-    .setAction(async ({add, run, imported}, hre, runSuper) => {
+    .addOptionalPositionalParam("action", "The action to execute: add, remove, list or run")
+    .addFlag("forceNonInteractive", "Raise an error if one or more params were not specified and the action would become interactive")
+    .addFlag("external", "Tells, for add/remove, that the module comes from an external package")
+    .addOptionalParam("module", "Tells the module to add/remove")
+    .setAction(async ({action, forceNonInteractive, external, module}, hre, runSuper) => {
         try {
-            add = (add || "").trim();
-            run = !!(run || !add);
-            // Please note: there's only ONE deployment file: ignition/deploy-everything.json.
-            // TODO 1. Get the chainId (string from base-10) from the current network.
-            //         This only makes sense when running, not when only --add is set.
-            // TODO 2. For --add ... --imported, take the ... file as good. It comes
-            //         from an external package, actually. If not present / provided,
-            //         ask for it via prompt and ensure it CAN be imported and it is
-            //         not already included in our deployment file.
-            // TODO 3. For --add ... without --imported, take the ... file. Then also
-            //         take all the .js files inside the project's ignition/modules
-            //         directory, except those files (from that directory) already in
-            //         the deployment file. If the file is not provided or not among
-            //         the options, complain and prompt the user to choose an option.
-            //         Otherwise, take it.
-            // TODO 4. Taking the file (and telling whether it is imported or not),
-            //         add the entry to the deployment file.
-            // TODO 5. If run: run the deployment file.
+            action = await chooseAction(action, forceNonInteractive, hre);
+            switch(action)
+            {
+                case "add":
+                    await add(module, external, forceNonInteractive, hre);
+                    break;
+                case "remove":
+                    await remove(module, external, forceNonInteractive, hre);
+                    break;
+                case "list":
+                    await list(hre);
+                    break;
+                case "run":
+                    await run(hre);
+                    break;
+                default:
+                    console.error("Invalid action: " + action);
+            }
         } catch(e) {
-            console.log(e);
+            console.error(e);
         }
     });
