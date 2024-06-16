25883b50a542efe4e6eb05699242c2422a3c8fc9
Luis Masuelli <luisfmasuelli@gmail.com>
2024-06-15 21:21:11 -0500

    Adding the import of parseChainId.

--- src/tasks/generate-deployment.js
+++ src/tasks/generate-deployment.js
@@ -8,6 +8,7 @@ const {inputUntil} = require("../utils/input");
 const {collectContractNames} = require("../utils/contracts");
 const {parseAddress} = require("../utils/accounts");
 const {checkNotInteractive} = require("../utils/common");
+const {parseChainId} = require("../utils/chains");
 
 
 /**
--- src/utils/chains.js
+++ src/utils/chains.js
@@ -4,9 +4,14 @@
  * @returns {number} The chain id as number.
  */
 function parseChainId(chainId) {
-    if (/^[1-9][\d*]$/.test(chainId)) {
+    if (/^[1-9]\d*$/.test(chainId)) {
         return parseInt(chainId);
     } else {
         throw new Error("Invalid chain id. Must be a base-10 positive number.");
     }
+}
+
+
+module.exports = {
+    parseChainId
 }
