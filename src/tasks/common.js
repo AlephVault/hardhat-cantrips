544741907dbf1bc536eb85ab6f7138a089a76dfc
Luis Masuelli <luisfmasuelli@gmail.com>
2024-06-17 16:09:40 -0500

    Ensuring the setAction function has a previous call to check the project uses ethers.js plug-in for hardhat.

--- src/tasks/common.js
+++ src/tasks/common.js
@@ -2,6 +2,24 @@ const {scope} = require("hardhat/config");
 const scope_ = scope("cantrips", "Many generators and quick helpers");
 
 
+/**
+ * Requires the ethers.js provider in the environment.
+ * @param hre The hardhat runtime environment.
+ */
+function requireEthersProvider(hre) {
+    if (!hre.ethers) {
+        throw new Error("This plug-in is only supported for projects using hardhat-ethers");
+    }
+}
+
+
+scope_.setAction_ = scope_.setAction
+scope_.setAction = (args, hre, ...more) => {
+    requireEthersProvider(hre);
+    return scope_.setAction(args, hre, ...more);
+}
+
+
 module.exports = {
-    cantripsScope: scope_
+    cantripsScope: scope_, requireEthersProvider
 }
