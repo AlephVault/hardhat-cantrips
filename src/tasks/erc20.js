7ea8571ff548c8901a3ef227580d642a82476e31
Luis Masuelli <luisfmasuelli@gmail.com>
2024-06-18 21:36:20 -0500

    Adding the mint method.

--- src/tasks/erc20.js
+++ src/tasks/erc20.js
@@ -107,3 +107,45 @@ cantripsScope.task("erc20:transfer", "Transfers an amount of tokens to another a
             console.error(e);
         }
     });
+
+
+cantripsScope.task("erc20:mint", "Mints an amount of tokens to another account or address")
+    .addOptionalParam("amount", "The amount to send, e.g. 1eth, 2.5eth or 3000000000000000000")
+    .addOptionalParam("toAddress", "The index (0 to number of accounts - 1), or address, of the account to send ETH to")
+    .addOptionalParam("usingAccount", "The index (0 to number of accounts - 1) of the account to send ETH from")
+    .addOptionalParam("contractId", "A contract id, specified as DeploymentModule#ContractId for the current network")
+    .addOptionalParam("deploymentId", "The deployment id to get the contract from (it MUST match the current network)")
+    .addFlag("forceNonInteractive", "Raise an error if one or more params were not specified and the action would become interactive")
+    .setAction(async ({contractId, deploymentId, forceNonInteractive, usingAccount, toAddress, amount}, hre, runSuper) => {
+        try {
+            // Get the contract.
+            const contract = await getDeployedContract(deploymentId, await selectDeployedContract(
+                contractId, deploymentId, forceNonInteractive, hre), hre
+            );
+
+            // Get the transfer parameters.
+            const usingAccount_ = await parseAccount(usingAccount || "0", hre);
+            const toAddress_ = await parseSmartAddress(toAddress || await inputUntil(
+                "0", "Insert checksum address or account index:", (v) => {
+                    return /^(\d+)|(0x[a-fA-F0-9]{40})$/.test(v);
+                }, "The value is not a valid address or account index", forceNonInteractive
+            ), hre);
+            const amount_ = parseAmount(amount || await inputUntil(
+                "1eth", "Insert amount (e.g. 1500000000000000000 or 1.5eth)", (v) => {
+                    return /^(\d+(\.\d+)?)\s*eth$/i.test(v.trim());
+                }, "The given amount is not valid", forceNonInteractive
+            ), hre);
+
+            // Perform the transfer.
+            await contract.connect(usingAccount_).mint(toAddress_, amount_);
+        } catch(e) {
+            console.error(
+                "Could not mint the tokens. This might happen because of many reasons, e.g. " +
+                "the contract does not implement ERC20 properly, the contract does not define " +
+                "mint(address, uint256) properly (or you don't meet the conditions to call it), " +
+                "or you have a deployment error (e.g. an invalid address or corrupted deployment " +
+                "files). Check the error for more details:"
+            )
+            console.error(e);
+        }
+    });
