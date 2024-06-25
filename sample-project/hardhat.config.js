3fa610c805e81c525d52cdebe6e9295134f34cd1
Luis Masuelli <luisfmasuelli@gmail.com>
2024-06-24 22:53:58 -0500

    Adding some settings to the hardhat.config.js file related to optimizer usage in Solidity and viaIR.

--- sample-project/hardhat.config.js
+++ sample-project/hardhat.config.js
@@ -4,5 +4,31 @@ require("..");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  // THIS SETTING OF SOLIDITY IS THE MOST IMPORTANT THING
+  // IN OUR PROJECT(S) SINCE OTHERWISE NO OPTIMIZER IS IN
+  // USE AND THE CHAINLINK CONTRACTS WILL FAIL ON BUILD.
+  //
+  // Alternatively, you can have Hardhat to use a modern
+  // version (e.g. 0.8.24) and reserve 0.8.19 (the lowest
+  // version to use with Chainlink contracts) to use the
+  // optimizer settings, so you don't lose stacktraces in
+  // the other contracts when debugging them.
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            details: {
+              yulDetails: {
+                optimizerSteps: "u"
+              }
+            }
+          }
+        }
+      }
+    ]
+  }
 };
