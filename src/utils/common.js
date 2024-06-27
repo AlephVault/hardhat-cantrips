b038f18c2f687243d82668103981d954a388174a
Luis Masuelli <luisfmasuelli@gmail.com>
2024-06-26 20:54:21 -0500

    Adding a comment.

--- src/utils/common.js
+++ src/utils/common.js
@@ -79,6 +79,10 @@ function normalizeByProjectPrefix(file, hre) {
 }
 
 
+/**
+ * Raises an error when forceNonInteractive is true.
+ * @param forceNonInteractive The force-non-interactive setting.
+ */
 function checkNotInteractive(forceNonInteractive) {
     if (forceNonInteractive) throw new Error("Aborting: The current action became interactive");
 }
